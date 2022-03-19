270899a8b1ca256caadae0818e49fa3a79d49e8c
CLHEX <clhexftw@protonmail.com>
2022-03-19 13:05:50 +0530

    ares: make fastbootd
    Signed-off-by: CLHEX <clhexftw@protonmail.com>

--- device.mk
+++ device.mk
@@ -11,6 +11,11 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Dynamic partitions setup
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock
+    
 # Filesystem
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/Android.mk:$(TARGET_COPY_OUT_RAMDISK)/fstab.default 

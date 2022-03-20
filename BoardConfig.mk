60c509beca605ef5a9823dba692240b9de94dd95
CLHEX <clhexftw@protonmail.com>
2022-03-20 14:32:43 +0530

    ares: Included MediaTek Sepolicy vndr
    Signed-off-by: CLHEX <clhexftw@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USES_MKE2FS := true
 
 # Sepolicy
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 
 # Verified Boot
